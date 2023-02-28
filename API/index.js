4eef92e6b930d24ab269a001709ebb5f8acf2852
unknown <tomygarnero9429@gmail.com>
2023-02-28 15:48:01 -0300

    Connecting to MongoDB

--- API/index.js
+++ API/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 // Crear el servidor
 const app = express();
+
+// Conectar a MongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/veterinaria", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
 
 // Puerto y arrancar el servidor
 app.listen(4000, () => {
